fccb6df13471a8eb2ea04a356d6b063aaf9272ac
BarryIse <amusaniseoluwa14@gmail.com>
2023-03-30 05:53:12 +0100

    update

--- index.php
+++ index.php
@@ -1,52 +1,6 @@
-<!doctype html>
-<html lang="en">
+<?php include 'inc/header.php'; ?>
 
 
-<head>
-
-    <title>Rent_a_lot</title>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Disable tap highlight on IE -->
-    <meta name="msapplication-tap-highlight" content="no">
-
-    <!-- Web Application Manifest -->
-
-    <!-- Add to home screen for Chrome on Android -->
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="application-name" content="xDocs">
-    <link rel="icon" sizes="192x192" href="assets/img/basic/chrome-touch-icon-192x192.png">
-
-    <!-- Add to home screen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="Web Starter Kit">
-    <link rel="apple-touch-icon" href="assets/img/basic/apple-touch-icon.png">
-
-    <!-- Tile icon for Win8 (144x144 + tile color) -->
-    <meta name="msapplication-TileImage" content="assets/img/basic/ms-touch-icon-144x144-precomposed.png">
-    <meta name="msapplication-TileColor" content="#55acee">
-    <!-- Color the status bar on mobile devices -->
-    <meta name="theme-color" content="#55acee">
-
-    <!--========================================
-    CSS
-    ===========================================-->
-    <!--3rd party plugins-->
-    <link href="assets/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="assets/lib/slick-carousel/slick/slick.css" rel="stylesheet" type="text/css">
-    <!--custom icons for classified website-->
-    <link href="assets/css/adspoticons.css" rel="stylesheet" type="text/css">
-    <!--main styles for template-->
-    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
-    <!--put your custom css on the file below-->
-    <link href="assets/css/custom.css" rel="stylesheet" type="text/css">
-
-</head>
 <body>
     <div class="web-app">
 
@@ -343,7 +297,7 @@
                             <div class="text-widget">
                                 <div class="inner">
                                     <h4>About Us</h4>
-                                    <p>As a leading entity in oline rental services. We thrive to ensure the satisfaction of all parties.</p>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis quaerat quibusdam animi id minus temporibus nesciunt culpa aut voluptate eius?</p>
                                 </div>
                             </div>
                         </div>
@@ -351,7 +305,7 @@
                             <div class="text-widget">
                                 <div class="inner">
                                     <h4>Rent Safely</h4>
-                                    <p>We guarantee the safety of your rented items.</p>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis quaerat quibusdam animi id minus temporibus nesciunt culpa aut voluptate eius?</p>
                                 </div>
                             </div>
                         </div>
@@ -359,7 +313,7 @@
                             <div class="text-widget">
                                 <div class="inner">
                                     <h4>How can we help</h4>
-                                    <p>You can lay any complains to our customers care services</p>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis quaerat quibusdam animi id minus temporibus nesciunt culpa aut voluptate eius?</p>
                                 </div>
                             </div>
                         </div>
